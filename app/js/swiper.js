95f7c1198d9adf5f4e28e3ec339d1abf90c17d89
NikolayTolkovanov-front <tnrvlg@mail.ru>
2022-05-30 23:15:16 +0300

    Стрелки для адаптива staking

--- app/js/swiper.js
+++ app/js/swiper.js
@@ -1,12 +1,12 @@
-const heroSwiper = new Swiper('.hero__swiper', {
+const heroSwiper = new Swiper(".hero__swiper", {
     slidesPerView: 4,
     allowTouchMove: false,
     spaceBetween: 30,
-    wrapperClass: 'hero__swiper-wrapper',
+    wrapperClass: "hero__swiper-wrapper",
 
     navigation: {
-        nextEl: '.hero__cards-btn--next',
-        prevEl: '.hero__cards-btn--prev'
+        nextEl: ".hero__cards-btn--next",
+        prevEl: ".hero__cards-btn--prev",
     },
 
     breakpoints: {
@@ -18,7 +18,6 @@ const heroSwiper = new Swiper('.hero__swiper', {
         970: {
             slidesPerView: 3,
             allowTouchMove: true,
-            
         },
 
         670: {
@@ -34,34 +33,62 @@ const heroSwiper = new Swiper('.hero__swiper', {
         320: {
             slidesPerView: 1,
             allowTouchMove: true,
-        }
-    }
-})
+        },
+    },
+});
 
-const stakingSwiper = new Swiper('.staking__swiper', {
+const stakingSwiper = new Swiper(".staking__swiper", {
     slidesPerView: 5,
     allowTouchMove: false,
-    wrapperClass: 'staking__swiper-wrapper',
-})
+    wrapperClass: "staking__swiper-wrapper",
+    navigation: {
+        nextEl: ".staking__graphic-btn--next",
+        prevEl: ".staking__graphic-btn--prev",
+    },
+    breakpoints: {
+        780: {
+            slidesPerView: 5,
+            allowTouchMove: true,
+        },
 
-const roundsSwiper = new Swiper('.rounds__swiper', {
+        670: {
+            slidesPerView: 3.5,
+            allowTouchMove: true,
+        },
+
+        460: {
+            slidesPerView: 2.2,
+            allowTouchMove: true,
+        },
+
+        320: {
+            slidesPerView: 1.5,
+            allowTouchMove: true,
+        }
+    },
+
+
+    // }
+});
+
+const roundsSwiper = new Swiper(".rounds__swiper", {
     slidesPerView: 5,
     grid: {
         rows: 2,
-        fill: 'col'
+        fill: "col",
     },
     spaceBetween: 25,
     allowTouchMove: false,
-    wrapperClass: 'rounds__swiper-wrapper',
-})
+    wrapperClass: "rounds__swiper-wrapper",
+});
 
-const kibotronSwiper = new Swiper('.kibotron__swiper', {
+const kibotronSwiper = new Swiper(".kibotron__swiper", {
     slidesPerView: 4,
     grid: {
         rows: 2,
-        fill: 'col'
+        fill: "col",
     },
     spaceBetween: 35,
     allowTouchMove: false,
-    wrapperClass: 'kibotron__swiper-wrapper'
-})
+    wrapperClass: "kibotron__swiper-wrapper",
+});
