c07afbe165651b04e7c734a552f99d298d62b334
NikolayTolkovanov-front <tnrvlg@mail.ru>
2022-06-12 20:17:44 +0300

    Адаптив для rounds

--- app/js/swiper.js
+++ app/js/swiper.js
@@ -1,12 +1,11 @@
-const heroSwiper = new Swiper(".hero__swiper", {
+const heroSwiper = new Swiper('.hero__swiper', {
     slidesPerView: 4,
     allowTouchMove: false,
     spaceBetween: 30,
-    wrapperClass: "hero__swiper-wrapper",
+    wrapperClass: 'hero__swiper-wrapper',
 
     navigation: {
-        nextEl: ".hero__cards-btn--next",
-        prevEl: ".hero__cards-btn--prev",
+        nextEl: '.hero__cards-btn--next',
     },
 
     breakpoints: {
@@ -37,13 +36,13 @@ const heroSwiper = new Swiper(".hero__swiper", {
     },
 });
 
-const stakingSwiper = new Swiper(".staking__swiper", {
+const stakingSwiper = new Swiper('.staking__swiper', {
     slidesPerView: 5,
     allowTouchMove: false,
-    wrapperClass: "staking__swiper-wrapper",
+    wrapperClass: 'staking__swiper-wrapper',
     navigation: {
-        nextEl: ".staking__graphic-btn--next",
-        prevEl: ".staking__graphic-btn--prev",
+        nextEl: '.staking__graphic-btn--next',
+        prevEl: '.staking__graphic-btn--prev',
     },
     breakpoints: {
         780: {
@@ -64,31 +63,82 @@ const stakingSwiper = new Swiper(".staking__swiper", {
         320: {
             slidesPerView: 1.5,
             allowTouchMove: true,
-        }
+        },
     },
-
 
     // }
 });
 
-const roundsSwiper = new Swiper(".rounds__swiper", {
+const roundsSwiper = new Swiper('.rounds__swiper', {
     slidesPerView: 5,
     grid: {
         rows: 2,
-        fill: "col",
+        fill: 'col',
     },
     spaceBetween: 25,
     allowTouchMove: false,
-    wrapperClass: "rounds__swiper-wrapper",
+    wrapperClass: 'rounds__swiper-wrapper',
+
+    navigation: {
+        nextEl: '.rounds__cards-btn--next',
+    },
+
+
+    breakpoints: {
+        960: {
+            slidesPerView: 5,
+            allowTouchMove: false,
+            grid: {
+                rows: 2,
+                fill: 'col',
+            },
+        },
+
+        794: {
+            slidesPerView: 4,
+            allowTouchMove: true,
+            grid: {
+                rows: 2,
+                fill: 'col',
+            },
+        },
+
+        680: {
+            slidesPerView: 3,
+            allowTouchMove: true,
+            grid: {
+                rows: 2,
+                fill: 'col',
+            },
+        },
+
+        480: {
+            slidesPerView: 2,
+            allowTouchMove: true,
+            grid: {
+                rows: 1,
+                fill: 'row',
+            },
+        },
+
+        320: {
+            slidesPerView: 1,
+            allowTouchMove: true,
+            grid: {
+                rows: 1,
+                fill: 'row',
+            },
+        },
+    },
 });
 
-const kibotronSwiper = new Swiper(".kibotron__swiper", {
+const kibotronSwiper = new Swiper('.kibotron__swiper', {
     slidesPerView: 4,
     grid: {
         rows: 2,
-        fill: "col",
+        fill: 'col',
     },
     spaceBetween: 35,
     allowTouchMove: false,
-    wrapperClass: "kibotron__swiper-wrapper",
+    wrapperClass: 'kibotron__swiper-wrapper',
 });
